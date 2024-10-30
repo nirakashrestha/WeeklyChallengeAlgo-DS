633110c2412877b698644748650028f04cb73e30
ROHAN\laptop <krb.rohan@gmail.com>
2024-10-30 18:12:38 -0400

    Merger

--- Solutions/Rohan/src/Merge.java
+++ Solutions/Rohan/src/Merge.java
@@ -1,4 +1,34 @@
-
+import java.util.Arrays;
 
 public class Merge {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+        System.out.println("Merged array: " + Arrays.toString(nums1));
+    }
+
+    public static void main(String[] args) {
+        Merge merger = new Merge();
+
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        merger.merge(nums1, m, nums2, n);
+
+       /* int[] nums1 = {1};
+        int m = 1;
+        int[] nums2= {0};
+        int n = 0;
+        merger.merge(nums1, m, nums2, n);*/
+
+
+        /*int[] nums1 = {0};
+        int m = 0;
+        int[] nums2 = {1};
+        int n = 1;
+        merger.merge(nums1, m, nums2, n);*/
+    }
 }
