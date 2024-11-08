94e86237a458065dfe5397aeb633153f54e0e6d2
Nirakash Shrestha <nirakash.shrestha@gmail.com>
2024-11-07 19:37:11 -0500

    solution valid parenthesis problem

---
+++ Solutions/Nirakash/ValidParenthesis.java
@@ -0,0 +1,35 @@
+package Solutions.Nirakash;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+    public static void main(String[] args) {
+        String s = "[([]){}({[]})]";
+
+        boolean is_stack_valid = isValid(s);
+
+        System.out.print(is_stack_valid);
+
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
